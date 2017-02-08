1d258388d2d34f35bd0121043797625b2ba3f4a0
irynakoles <irynakolesnikova1993@gmail.com>
2017-02-08 10:30:13 +0200

    Initial commit

---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int firstNumber;
+            int secondNumber;
+
+            //Entering  and validating first number
+            Console.WriteLine("Enter first number: ");
+            try
+            {
+                userInput = Console.ReadLine();
+                firstNumber = Convert.ToInt32(userInput);
+                // Entering and validating second number
+                Console.WriteLine("Enter second number: ");
+                userInput = Console.ReadLine();
+                secondNumber = Convert.ToInt32(userInput);
+
+                //Check if first number devided by second without remainder
+                if (firstNumber % secondNumber == 0)
+                {
+                    Console.WriteLine("First number divide by second number without remainder ");
+                }
+                else
+                {
+                    Console.WriteLine("First number devide by second number with remainder ");
+                }
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
