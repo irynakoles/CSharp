5a5bab3dfc3813110133c9c2520f9ffbbde686bc
irynakoles <irynakolesnikova1993@gmail.com>
2017-02-08 10:58:15 +0200

    Initial commit

---
+++ Lesson_2/Lesson_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for( i=1; i<=100; i++)
+            {
+                if(i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((i%3==0) && (i%5==0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadKey();        
+            }
+        }
+    }
+}
