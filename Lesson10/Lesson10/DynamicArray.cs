2fe15d5418791d4fcd03d7e94bbb9d08c1c27013
irynakoles <irynakolesnikova1993@gmail.com>
2017-03-07 22:15:16 +0200

    Updates

---
+++ Lesson10/Lesson10/DynamicArray.cs
@@ -0,0 +1,78 @@
+﻿namespace Lesson10
+{
+    public class DynamicArray<T>
+    {
+        private int size;
+        private int capacity;
+        private T[] array;
+
+        public DynamicArray()
+        {
+            capacity = 1;
+            size = 0;
+            array = new T[capacity];
+        }
+
+        public void Remove(int index)
+        {
+            for (int i = index; i < size; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+
+        public void Add(T i)
+        {
+            if (size == (capacity - 1))
+            {
+                IncreaseCapacity();
+            }
+            array[size] = i;
+            size++;
+        }
+
+        public void Insert(T insert, int index)
+        {
+            if (size == (capacity - 1))
+            {
+                IncreaseCapacity();
+            }
+            for (int i = index; i < size; i++)
+            {
+                array[i + 1] = array[i];
+
+            }
+            array[index] = insert;
+            size++;
+        }
+
+        public T Get(int index)
+        {
+            T value = array[index];
+            return value;
+        }
+
+        public void IncreaseCapacity()
+        {
+            capacity *= 2;
+            T[] secondArray = new T[capacity];
+            for (int i = 0; i < array.Length; i++)
+            {
+                secondArray[i] = array[i];
+            }
+            array = secondArray;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public int GetCapacity()
+        {
+            return capacity;
+        }
+    }
+}
+
---
+++ Lesson10/Lesson10/DynamicQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lesson10
+{
+    public class DynamicQueue<T>
+    {
+        private DynamicArray<T> queue;
+        private int begin = 0;
+
+        public DynamicQueue()
+        {
+            queue = new DynamicArray<T>();
+        }
+
+        public void Engueue(T value)
+        {
+            queue.Add(value);
+        }
+
+        public T Dequeue()
+        {
+            T value = queue.Get(begin);
+            queue.Remove(begin);
+            return value;
+        }
+
+        public void EnqueueByIndex(T value, int index)
+        {
+            queue.Insert(value, index);
+        }
+
+        public void PrintQueue()
+        {
+            Console.WriteLine("Queue: ");
+            for (int i = 0; i < queue.GetSize(); i++)
+            {
+                Console.WriteLine(queue.Get(i));
+            }
+        }
+        public void PrintCapacity()
+        {
+            Console.WriteLine($"Capacity: {queue.GetCapacity()}");
+        }
+
+        public void PrintSize()
+        {
+            Console.WriteLine($"Size: {queue.GetSize()}");
+        }
+
+        public bool IsFull()
+        {
+            return queue.GetCapacity() == queue.GetSize();
+
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.GetSize() == 0;
+
+        }
+    }
+}
+
